ba161a500ce10ddfb2a0569577e3a59c603aded6
SOU61204 <souhardya6.12.2004@gmail.com>
2026-07-05 18:27:46 +0530

    Fix: Add CORSMiddleware to allow frontend connections

--- backend/server.py
+++ backend/server.py
@@ -36,7 +36,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "https://music-key-analyser-frontend.onrender.com",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
